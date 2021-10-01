951b68faff11641db37305d0b20a56eb38994a7c
Lucas Seijo <lucaseijo7@gmail.com>
2021-10-01 20:47:49 -0300

    comenzando a utilizar components

--- js/app.js
+++ js/app.js
@@ -1,56 +1,13 @@
-const Foo = { template: '<div>foo</div>' };
-const Bar = { template: '<div>bar</div>' };
-
-const routes = [
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
-  ];
-
 const router = new VueRouter({
-    routes // short for `routes: routes`
+    base: 'Vue Demo',
+    routes: [
+        {
+            path: '/lists',
+            component: Lists
+    }]
 });
 
 const app = new Vue({
     el: '#app',
-    router,
-    data: {
-        message: 'Hello Vue',
-        sum: 3 + 6,
-        isVisible: true,
-        people: ['Paula', 'Lucas', 'Carmelo'],
-        html_content: '<h1>Hello Vue!</h1>',
-        src: 'https://www.arsys.es/blog/file/uploads/2020/04/01-vuejs.jpg',
-        numero: 30,
-        p_error: true
-    },
-    computed: {
-        // a computed getter
-        reversedMessage: function () {
-            // `this` points to the vm instance
-            return this.message.split('').reverse().join('');
-        }
-    },
-    methods: {
-        calculateSum: function (value1, value2){
-            return value1 + value2;
-        },
-        decrementar: function() {
-            return this.numero--;
-        },
-        incrementar: function() {
-            return this.numero++;
-        },
-        cambiarError: function() {
-            if(this.p_error == true){
-                return this.p_error = false;
-            } else {
-                return this.p_error = true;
-            }
-        }
-    },
-    watch: {
-        numero: function (value) {
-            console.log(value);
-        }
-    }
-})
+    router
+});
