f8ab0418c3f8faa2272e120064ddeca83ee1a124
Lucas Seijo <lucaseijo7@gmail.com>
2021-10-01 12:28:24 -0300

    se agregan computed y methods

--- js/app.js
+++ js/app.js
@@ -3,5 +3,17 @@ const app = new Vue({
     data: {
         message: 'Hello Vue',
         sum: 3 + 6
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function () {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('');
+        }
+    },
+    methods: {
+        calculateSum: function (value1, value2){
+            return value1 + value2;
+        }
     }
 })
