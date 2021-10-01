4112669c07fbf0f1ebf47ed4aa2e9788ffae0ecb
Lucas Seijo <lucaseijo7@gmail.com>
2021-10-01 17:57:55 -0300

    probando router

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+const Foo = { template: '<div>foo</div>' };
+const Bar = { template: '<div>bar</div>' };
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+  ];
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+});
+
 const app = new Vue({
     el: '#app',
+    router,
     data: {
         message: 'Hello Vue',
         sum: 3 + 6,
@@ -7,7 +20,8 @@ const app = new Vue({
         people: ['Paula', 'Lucas', 'Carmelo'],
         html_content: '<h1>Hello Vue!</h1>',
         src: 'https://www.arsys.es/blog/file/uploads/2020/04/01-vuejs.jpg',
-        numero: 30
+        numero: 30,
+        p_error: true
     },
     computed: {
         // a computed getter
@@ -20,11 +34,23 @@ const app = new Vue({
         calculateSum: function (value1, value2){
             return value1 + value2;
         },
-        decrementar: function () {
+        decrementar: function() {
             return this.numero--;
         },
-        incrementar: function () {
+        incrementar: function() {
             return this.numero++;
+        },
+        cambiarError: function() {
+            if(this.p_error == true){
+                return this.p_error = false;
+            } else {
+                return this.p_error = true;
+            }
+        }
+    },
+    watch: {
+        numero: function (value) {
+            console.log(value);
         }
     }
 })
